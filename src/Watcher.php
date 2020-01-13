ada3f697968cfee350ae5072e6a4f0763eab6751
huang-yi <coodeer@163.com>
2020-01-13 17:50:38 +0800

    Finish the watcher

--- src/Watcher.php
+++ src/Watcher.php
@@ -2,16 +2,33 @@
 
 namespace HuangYi\Watcher;
 
+use Closure;
 use HuangYi\Watcher\Contracts\Command;
+use HuangYi\Watcher\Contracts\Watcher as WatcherContract;
+use Swoole\Process;
 
-class Watcher
+class Watcher implements WatcherContract
 {
     /**
-     * Get the executable command.
+     * The command instance.
      *
      * @var \HuangYi\Watcher\Contracts\Command
      */
     protected $command;
+
+    /**
+     * The process instance.
+     *
+     * @var \Swoole\Process
+     */
+    protected $process;
+
+    /**
+     * The change event callback.
+     *
+     * @var \Closure
+     */
+    protected $onChangeCallback;
 
     /**
      * Watcher constructor.
@@ -22,10 +39,98 @@ class Watcher
     public function __construct(Command $command)
     {
         $this->command = $command;
+
+        $this->init();
     }
 
+    /**
+     * Initialize.
+     *
+     * @return void
+     */
+    protected function init()
+    {
+        $this->process = new Process(function ($process) {
+            $process->exec(...$this->command->getCommand());
+        }, true);
+    }
+
+    /**
+     * Start the watcher.
+     *
+     * @return void
+     */
     public function watch()
     {
+        $this->process->start();
 
+        swoole_event_add($this->process->pipe, function () {
+            $outputs = $this->process->read();
+
+            if ($callback = $this->getOnChangeCallback()) {
+                call_user_func($callback, $outputs);
+            }
+        });
+    }
+
+    /**
+     * Get the callback.
+     *
+     * @return \Closure
+     */
+    public function getOnChangeCallback()
+    {
+        if (! $this->onChangeCallback) {
+            return null;
+        }
+
+        return function ($outputs) {
+            $events = $this->command->parseEvents($outputs);
+
+            call_user_func($this->onChangeCallback, $events);
+        };
+    }
+
+    /**
+     * Register a change event callback.
+     *
+     * @param  \Closure  $callback
+     * @return $this
+     */
+    public function onChange(Closure $callback)
+    {
+        $this->onChangeCallback = $callback;
+
+        return $this;
+    }
+
+    /**
+     * Stop the watcher.
+     *
+     * @return void
+     */
+    public function stop()
+    {
+        $this->process->exit();
+    }
+
+    /**
+     * Get the command instance.
+     *
+     * @return \HuangYi\Watcher\Contracts\Command
+     */
+    public function getCommand()
+    {
+        return $this->command;
+    }
+
+    /**
+     * Get the process instance.
+     *
+     * @return \Swoole\Process
+     */
+    public function getProcess()
+    {
+        return $this->process;
     }
 }
